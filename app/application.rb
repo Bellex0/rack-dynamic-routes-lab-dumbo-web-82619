91d8259f6476529c943dc19534ef4d0b416886eb
Belle P <bellepx3@gmail.com>
2019-09-15 22:59:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
  
     if req.path.match(/items/)
     search_term = req.params["ITEM NAME"]
-      resp.write 
+      resp.write search_term.price
     else
       resp.write "Route not found"
       resp.status = 404
