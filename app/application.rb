eb1a3824e1fc064c44eb532ad792b65a6a4acb19
Belle P <bellepx3@gmail.com>
2019-09-15 23:02:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,10 @@ class Application
     search_term = req.params["ITEM NAME"]
     if @@items.include?(search_term)
       resp.write "#{search_term.price}"
+      resp.status = 200
     else
       resp.write "Item not found"
+      resp.status = 400
     else 
       resp.write "Route not found"
       resp.status = 404
